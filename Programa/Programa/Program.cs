65abbb733a4955769d2278bc01ed37c5ae0c0610
TamyresSteffany <tamyresschimite@gmail.com>
2023-03-14 10:08:51 -0400

    pudim de leite

---
+++ Programa/Programa/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Programa
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("\t\t--- Vererificar se o numero é impar ou par---\n");
+            Console.Write("informe um numero:");
+            int num = int.Parse(Console.ReadLine());
+
+            if (num % 2 == 0)
+            {
+                Console.WriteLine("\nEste numero é par!");
+            }
+            else
+            {
+                Console.WriteLine("\nEste numero é impar!");
+            }
+            Console.ReadKey();
+        }
+    }
+}
